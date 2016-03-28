b259da8dca9be52340ea7bc1c31ec2d569211cde
WeiAiTingYu <1270606966@qq.com>
2016-03-28 19:57:10 +0800

    c#
    作业

---
+++ CSharp1/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace CSharp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(68, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "打开文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(107, 63);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(321, 135);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(219, 17);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 27);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "去除特殊字符";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(552, 17);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(209, 53);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "轮播路径选择";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(548, 116);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(264, 179);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 3000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(325, 16);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(103, 24);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "另存为";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(894, 378);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ CSharp1/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+namespace CSharp1
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog fileDialog;
+        FolderBrowserDialog folder;
+        String[] list;
+        int i = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Show();
+           
+        }
+ 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+            //folderDialog.ShowDialog();
+            fileDialog = new OpenFileDialog();
+            fileDialog.InitialDirectory = @"C:\Users\王小兵\Documents\Visual Studio 2015\Projects\CSharp1\CSharp1";
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader sr = new StreamReader(fileDialog.FileName, Encoding.UTF8);
+                try {
+                    textBox1.Text = sr.ReadToEnd();
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    sr.Close();
+                }
+               
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // String m= @"[^`~!@#$%^&*()+=|{}':;',\\[\\].<>/?~！@#￥%……&*（）——+|{}【】‘；：”“’。，、？]";
+            String m = "[^！@#￥$#@&*]";
+            Regex regex = new Regex(m);
+            MatchCollection matchc= regex.Matches(textBox1.Text);
+            textBox1.Text = "";
+            for(int i = 0; i < matchc.Count; i++)
+            {
+                textBox1.Text += matchc[i].Value;
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+           folder = new FolderBrowserDialog();
+            //folder.RootFolder = Path.GetPathRoot(@"C:\Users\王小兵\Documents\VisualStudio2015\Projects\CSharp1\CSharp1");
+            folder.SelectedPath = @"C:\Users\王小兵\Documents\Visual Studio 2015\Projects\CSharp1\Csharp1";
+            folder.ShowDialog();
+            // imageList1.
+             if(folder.SelectedPath!="")
+             list = Directory.GetFiles(folder.SelectedPath);
+            folder.Dispose();
+            timer1.Enabled = true;
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (folder.SelectedPath!=null)
+            {
+                //  pictureBox1.ImageLocation = folder.SelectedPath;
+                pictureBox1.ImageLocation = list[i++];
+                if (i == list.Length) i = 0;
+            }
+
+          
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog savedia = new SaveFileDialog();
+          //  savedia.InitialDirectory= @"images";
+            savedia.Filter = "文本文件|*.txt|所有文件|*.*";
+            savedia.FilterIndex = 1;
+            if (savedia.ShowDialog() == DialogResult.OK)
+            {
+                RichTextBox t1 = new RichTextBox();
+                string path = fileDialog.FileName;
+                StreamWriter sw = new StreamWriter(savedia.FileName,false);
+                sw.Write(textBox1.Text,true);
+                sw.Close();
+                textBox1.Text = "";
+             }
+        }
+    }
+}
